0833d3a09c46f5c90b7a20add0245f0d3efeed8a
bikashsaud <saudbikash514@gmail.com>
2021-11-30 07:14:12 +0545

    create multiple component and user array from s data

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React  from "react";
+import ReactDOM from "react-dom";
+
+import Card from "./Card"
+import SData from "./series_data";
+import Sdata from './series_data'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+      <h1>TOP 10 Netflix Series</h1>
+
+    <Card 
+    imgsrc={SData[0]['imgsrc']}
+    title = {Sdata[0]['title']} 
+    sname={Sdata[0]['sname']}
+    link= {Sdata[0]['link']}
+  />
+    <Card 
+    imgsrc={SData[1]['imgsrc']}
+    title = {Sdata[1]['title']} 
+    sname={Sdata[1]['sname']}
+    link= {Sdata[1]['link']}
+  />
+      <Card 
+    imgsrc={SData[2]['imgsrc']}
+    title = {Sdata[2]['title']} 
+    sname={Sdata[2]['sname']}
+    link= {Sdata[2]['link']}
+  />
+      <Card 
+    imgsrc={SData[3]['imgsrc']}
+    title = {Sdata[3]['title']} 
+    sname={Sdata[3]['sname']}
+    link= {Sdata[3]['link']}
+  />
+
+  </>
+, document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+{/* <Card 
+  imgsrc="https://occ-0-4111-3663.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQ4HySPdUY-5TtRD7o9U4bJDhZ70maqF-fJ8jhjNc1GZGWa2s_MocCD1VEA9hmebxJwbTFHAZG6GobSlyIZYrUVfO-hjbc66r43ans0WEgp_ClnFZuqQOYYRRKts.jpg?r=02d"
+  title = "A Netflix Original Series"
+  sname="Helly Bound"
+  link="https://www.netflix.com/np/title/81256675"
+/>  
+
+<Card 
+    imgsrc="https://occ-0-4111-3663.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQCcYpChthxsZQ5WGbjtoPKANvAlzetRttqwMu3n5gl1D4jisccCMbgAcFfBmwXIOfjQeCdIzDZ47FAkX85kZQTUKcISpo3Uio8WEhFWXhhMhw3yUBx-Z0ymyCVd.jpg?r=cfa"
+    title = "A Netflix Original Series"
+    sname="Money Heist"
+    link="https://www.netflix.com/np/title/80192098"
+/>  
+
+<Card 
+    imgsrc="https://occ-0-4111-3663.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABatBKtC4V1JuLoCvbfq1JQG3K2Bui_h6Sp0PIEYr2MHkiQCta-DvZlBbMTIopXpCU_v6b56sOjmlIoEBuv0RLdEzjrQoIx70MqxhgEa8NK05yM_VrTzfQNfhgNvQ.jpg?r=d92"
+    title = "A Netflix Original Series"
+    sname="Lucifer"
+    link="https://www.netflix.com/np/title/80057918"
+/>   */}
---
+++ src/series_data.jsx
@@ -0,0 +1,30 @@
+const SData = [
+    {
+        imgsrc:"https://occ-0-4111-3663.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABS3om16T6zfR1N1qCSNpHAatBKJuiLulRr7IsbUAWS_IWxkVtdtOlI0QZLmluo1_UWh4P6i_iK4115PC38WCI2fTdwL4aO3D0fE49LMjoSHNPd8C11srecK0BrEp.jpg?r:f9b",
+        title : "A Netflix Original Series",
+        sname:"SQUID Game",
+        link:"https://www.netflix.com/np/title/81040344",
+    },
+    {
+    imgsrc:"https://occ-0-4111-3663.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQ4HySPdUY-5TtRD7o9U4bJDhZ70maqF-fJ8jhjNc1GZGWa2s_MocCD1VEA9hmebxJwbTFHAZG6GobSlyIZYrUVfO-hjbc66r43ans0WEgp_ClnFZuqQOYYRRKts.jpg?r=02d",
+    title : "A Netflix Original Series",
+    sname:"Helly Bound",
+    link:"https://www.netflix.com/np/title/81256675"
+
+    },{
+    imgsrc:"https://occ-0-4111-3663.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQCcYpChthxsZQ5WGbjtoPKANvAlzetRttqwMu3n5gl1D4jisccCMbgAcFfBmwXIOfjQeCdIzDZ47FAkX85kZQTUKcISpo3Uio8WEhFWXhhMhw3yUBx-Z0ymyCVd.jpg?r=cfa",
+    title : "A Netflix Original Series",
+    sname:"Money Heist",
+    link:"https://www.netflix.com/np/title/80192098"},
+    {
+        imgsrc:"https://occ-0-4111-3663.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABatBKtC4V1JuLoCvbfq1JQG3K2Bui_h6Sp0PIEYr2MHkiQCta-DvZlBbMTIopXpCU_v6b56sOjmlIoEBuv0RLdEzjrQoIx70MqxhgEa8NK05yM_VrTzfQNfhgNvQ.jpg?r=d9:",
+        title : "A Netflix Original Series",
+        sname:"Lucifer",
+        link:"https://www.netflix.com/np/title/80057918"
+    }
+]
+
+
+
+
+export default SData;
