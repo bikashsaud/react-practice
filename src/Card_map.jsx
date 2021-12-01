7ad47c1514da6626cdc78f0371e1d0ad3f236f5f
bikashsaud <saudbikash514@gmail.com>
2021-12-01 08:03:56 +0545

    minor change

--- src/Card_map.jsx
+++ src/Card_map.jsx
@@ -4,6 +4,7 @@ import Sdata from './series_data'
 function nCard(val){
     return (
       <Card 
+      key = {val.id}
       imgsrc={val.imgsrc}
       title = {val.title} 
       sname={val.sname}
