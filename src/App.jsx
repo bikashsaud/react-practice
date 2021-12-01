24ee360c5a2a539cd9111766b687d3dbe91df55f
bikashsaud <saudbikash514@gmail.com>
2021-12-01 13:40:52 +0545

    change for new component  and if else conditon

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import React  from "react";
 import Card from "./Card"
 import SData from "./series_data";
+import Netflix from "./UserFav/Netflix"
+import Amazon from "./UserFav/Amazon"
 
+const user_opt = "netflix";
 
+const FavSeries =() => {
 
+if (user_opt === "netflix")
+  {
+    return  <Netflix />
+  }
+  else{
+    return  <Amazon />
+  }
+
+}
 const  App = () => (
   <>
       <h1>TOP 10 Netflix Series</h1>
-      {SData.map((val, index)=>{
+      <FavSeries />
+      {/* thihs is for maping the value from sdata fie */}
+      {/* {SData.map((val, index)=>{
           console.log(index, 'index number')
         return(  
         <Card 
@@ -18,7 +33,7 @@ const  App = () => (
         link= {val.link}
       />
       )})
-      }
+      } */}
 
   </>
 );
