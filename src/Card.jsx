c8c34e96706c99ba15c4f3e4405a7d1603a2da55
bikashsaud <saudbikash514@gmail.com>
2021-12-01 08:03:30 +0545

    new component add in card file

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,17 +1,25 @@
 
 import React from "react";
+import Images from './Images'
+import Headings from './Headings'
 
 function Card(props){
     return(
       <>
     <div className="cards">
       <div className="card">
-        <img src={props.imgsrc} alt="myPic" className="card_img"/>
+        
+        <Images 
+          imgsrc = {props.imgsrc}
+         />
         <div className="card_info">
           <span className="card_category">
             {props.title}
           </span>
-        <h3 className="card_title">{props.sname}</h3>
+
+          <Headings 
+            heading = {props.sname}
+          />
         <a href={props.link} target="_blank">
           <button>Watch Now</button>
         </a>
